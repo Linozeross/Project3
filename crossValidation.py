48acc8d94361e817e0330173d76b8a9046478e53
Jorge Morales <>
2017-11-12 16:58:29 -0500

    Commented the code

--- crossValidation.py
+++ crossValidation.py
@@ -16,22 +16,19 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 import csv
 
-
+"""Calculates alpha given a learnin and epoch"""
 def learning_decay(epoch, initial_rate, decay=0.0005):
     rate=initial_rate * 1/(1 + decay * epoch)
     return rate
 
+"""Calculates the generalization loss"""
 def generalization_loss(epoch_error,min_error, gl_alpha=0.1):
      generalization_loss=100*((epoch_error/min_error)-1)
      print('gl='+str(generalization_loss))
      return  generalization_loss>gl_alpha or (generalization_loss>0 and math.isclose(generalization_loss, 0, abs_tol=1e-5))
          
-
+"""Finds the training epochs that provide the better results"""
 def tune_epochs(x, y, k, input_neurons, hidden_neurons, layers, output_neurons, alpha):
-    print("inputs: %d, hidden: %d, layers: %d, output: %d, alpha: %f"% (input_neurons,hidden_neurons,layers,output_neurons,alpha))
-    f=open('500_crossValidationResults.csv','a')
-    writer = csv.writer(f)
-    
     #Epochs
     training_epoch=0
     best_epoch=None
@@ -55,8 +52,6 @@ def tune_epochs(x, y, k, input_neurons, hidden_neurons, layers, output_neurons, 
                 NN=nets[index]
                 #trains the network
                 NN.train_network(x[train_index],y[train_index], learning_rate)
-                
-                
                 #gets prediction over validation
                 prediction=NN.predict_batch(x[validation_index])
                 #print(prediction)
@@ -68,10 +63,7 @@ def tune_epochs(x, y, k, input_neurons, hidden_neurons, layers, output_neurons, 
         epoch_error=np.average(valid_error)
         #Checks the stopping condition
         stop_condition=generalization_loss(epoch_error,min_val_error)
-        results=[input_neurons,hidden_neurons,layers,output_neurons,alpha,training_epoch,epoch_error]
-        writer.writerow(results)
-        print('epoch_error='+str(epoch_error))
-        
+        #Updates the min values
         if epoch_error < min_val_error:
             min_val_error= epoch_error
             best_epoch=training_epoch
@@ -81,14 +73,16 @@ def tune_epochs(x, y, k, input_neurons, hidden_neurons, layers, output_neurons, 
         #updates learning rate
         learning_rate=learning_decay(training_epoch,learning_rate)
     
-    f.close()
     #Returns the best epoch and the validation error
     return best_epoch, min_val_error
-    
+
+"""Finds the best learning rate among a set of random values"""
 def tune_alpha(x, y, k, input_neurons, hidden_neurons, layers, output_neurons):
     #Learning rates
-    N=2
+    N=10
     smaller_exp=-2
+
+    #Generates the random numbers
     learning_rates=np.power(10*np.ones(N),smaller_exp*np.random.random_sample((N,)))
     
     #learning rate
@@ -96,9 +90,11 @@ def tune_alpha(x, y, k, input_neurons, hidden_neurons, layers, output_neurons):
     best_alpha_error=np.inf
     best_epoch=None
     
+    #Iterates over the values to train the neural network 
     for learning_rate in learning_rates:
         epoch, epoch_error=tune_epochs(x,y,k,input_neurons,hidden_neurons,layers,output_neurons,learning_rate)
         
+        #Updates the minimum error
         if epoch_error < best_alpha_error:
             bestalpha=learning_rate
             best_epoch=epoch
@@ -106,6 +102,7 @@ def tune_alpha(x, y, k, input_neurons, hidden_neurons, layers, output_neurons):
     
     return best_epoch, bestalpha, best_alpha_error
 
+"""Finds the best number of neurons for the hidden layer"""
 def tune_neurons(x, y, k, input_neurons, layers, output_neurons):
     #learning rate
     best_number_neurons=None
@@ -115,6 +112,7 @@ def tune_neurons(x, y, k, input_neurons, layers, output_neurons):
      #Hidden Neurons
     neurons=int(log(input_neurons))
     
+    #While the error keeps on improving, keep increasing the neurons
     while True:
         epoch, alpha, error=tune_alpha(x,y,k,input_neurons,neurons,layers,output_neurons)
         
@@ -125,21 +123,19 @@ def tune_neurons(x, y, k, input_neurons, layers, output_neurons):
         best_epoch=epoch
         best_number_neurons=neurons
         best_error=error
-        
+
+                
         neurons+=1
+
     
     return best_epoch, best_alpha, best_number_neurons, best_error
-    
+
+"""Finds the best parameters for the NN"""
 def cross_validation(k,x,y):  
     #Input neurons
     inp=x[0].size
     #Output classes
     outp=40
-    f=open('500_crossValidationResults.csv','w')
-    results=["inputs","hidden","layers", "output", "alpha","epoch","validation_error"]
-    writer = csv.writer(f)
-    writer.writerow(results)
-    f.close()
     #Saves a portion of the data for testing
     X_train, Y_train, X_test, Y_test = preprocessing.spltDataset(x, y, 0.1,15)
     
@@ -153,6 +149,7 @@ def cross_validation(k,x,y):
      #Hidden layers
     layers=1
     
+    #While the result keeps on improving add more layers
     while True:
         epoch, alpha, neurons, error=tune_neurons(X_train,Y_train,k,inp,layers,outp)
         
@@ -170,6 +167,7 @@ def cross_validation(k,x,y):
     return  best_epoch, best_alpha, best_number_neurons, best_layers,best_error
         
 
+"""Train and test a NN for the given parameters"""
 def test_NN(X_train, Y_train, X_valid, Y_valid, test, layers, hidden_neurons, alpha, epochs,labels):
     f=open('NNTrainResults.csv','w')
     writer = csv.writer(f)
@@ -180,6 +178,7 @@ def test_NN(X_train, Y_train, X_valid, Y_valid, test, layers, hidden_neurons, al
     learning_rate=alpha
     
     NN=byhand.FFN(len(X_train[0]),hidden_neurons,layers,40)  
+    #Train one epoch at a time
     for epoch in range(0,epochs):
         NN.train_network(X_train,Y_train, learning_rate)
         #gets prediction over training
@@ -187,7 +186,6 @@ def test_NN(X_train, Y_train, X_valid, Y_valid, test, layers, hidden_neurons, al
         #gets prediction over training
         prediction=NN.predict_batch(X_valid)
         #Calculates validation error
-#        trainPrediction=
         err_train=mean_squared_error(Y_train, trainPrediction)
         err_val=mean_squared_error(Y_valid, prediction)
         train_error.append(err_train)
@@ -196,18 +194,15 @@ def test_NN(X_train, Y_train, X_valid, Y_valid, test, layers, hidden_neurons, al
         
         #updates learning rate
         learning_rate=learning_decay(epoch,learning_rate)
-            
         results=[err_train,err_val]
-        writer.writerow(results)
-        print('epoch_error= %f,%f'% (err_train,err_val))
         
-    f.close()
-    
+    #Gets the confussion matrix of the model    
     prediction_valid=NN.predict_batch(X_valid)
     labs= labels.inverse_transform(np.asarray(prediction_valid))
     y_true= labels.inverse_transform(np.asarray(Y_valid))
     test_matrix=confusion_matrix(y_true,labs)
     
+    #Predicts the test set labels
     prediction=NN.predict_batch(test)
     prediction= labels.inverse_transform(np.asarray(prediction))
 
@@ -215,38 +210,29 @@ def test_NN(X_train, Y_train, X_valid, Y_valid, test, layers, hidden_neurons, al
     
     
 
-#x = np.loadtxt("train_x_bin.csv", delimiter=",")
-#y = np.loadtxt("train_y.csv", delimiter=",")
-#test = np.loadtxt("test_x_bin.csv", delimiter=",")
-#
-#X_train, Y_train, X_valid, Y_valid = preprocessing.spltDataset(x, y, 0.1,15)
-#preprocessing.write_file("new_train_x",X_train)
-#preprocessing.write_file("new_train_y",Y_train)
-#preprocessing.write_file("new_valid_x",X_valid)
-#preprocessing.write_file("new_valid_y",Y_valid)
-
-X_train = np.loadtxt("new_train_x", delimiter=",")
-Y_train = np.loadtxt("new_train_y", delimiter=",")
-X_valid = np.loadtxt("new_valid_x", delimiter=",")
-Y_valid = np.loadtxt("new_valid_y", delimiter=",")
-test = np.loadtxt("test_x_bin.csv", delimiter=",")
+x = np.loadtxt("train_x.csv", delimiter=",")
+y = np.loadtxt("train_y.csv", delimiter=",")
+test = np.loadtxt("test_x.csv", delimiter=",")
 print("Finished loading")
 
+#Binarize the data
+x=preprocessing.binarize(x)
+test=preprocessing.binarize(test)
+
+#Binarize the labels
 lb=LabelBinarizer()
 Y_train=lb.fit_transform(Y_train)
 Y_valid=lb.transform(Y_valid)
-#epoch, alpha, neurons, layers,error=cross_validation(10,x,y_bin)
 
+#Makes the prediction
 prediction, confussion, valid_error,train_error=test_NN(X_train, Y_train, X_valid,Y_valid,test,1,8,0.085692543223609377,69,lb)
 
-preprocessing.write_file("confusion",confussion)
-preprocessing.write_file("valid_errors",valid_error)
-preprocessing.write_file("train_errors",train_error)
-
+#Writes the results
+preprocessing.write_file("confusion.csv",confussion)
 labels=np.array(['Id','Label'])
 ids=list(range(1,prediction.size+1))
 toSave=np.c_[ids,prediction.astype(int).astype(str)]
 toSave=np.vstack((labels,toSave))
-preprocessing.write_file('prediction_log_reg.csv',prediction)
+preprocessing.write_file('prediction_NN.csv',prediction)
 
 
